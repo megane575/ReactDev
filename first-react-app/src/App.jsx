5df2aa3aad04c9a36db69ca6703644035d6639d3
megane575 <rie-fttm0129@gmail.com>
2025-06-19 22:43:43 +0900

    App.jsxをコンポーネント化してTodoTemplate.jsxを作成

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,35 +1,8 @@
-import './App.css';
-import { useApp } from "./hooks/useApp";
-import { AddTodo } from "./components/AddTodo";
-import { TodoList } from "./components/TodoList";
-import { SearchWordTodo } from "./components/SearchedList"
+import { TodoProvider } from './contexts/TodoContext';
+import { TodoTemplate } from './components/templates/TodoTemplate';
 
 export const App = () => {
-    const [states,actions] = useApp();
-    return (
-        <div className="container">
-            <h1 className="title">Todo List</h1>
-            {/*Todo追加領域*/} 
-            <section className="common-area">
-              <AddTodo
-              addInputValue={states.addInputValue}
-              onChangeAddInputValue={actions.onChangeAddInputValue}
-              handleAddTodo={actions.handleAddTodo}
-              />
-            </section>
-             {/*Todo検索フォーム領域*/}
-            <section className="common-area"> 
-            < SearchWordTodo
-              searchKeyword={states.searchKeyword}
-              handleChangeSearchKeyword={actions.handleChangeSearchKeyword}
-              handleKeyDownSearch={actions.handleKeyDownSearch}
-            />
-            {/*Todo一覧表示領域*/}
-            < TodoList
-                todoList={actions.filteredTodoList}
-                handleDeleteTodo={actions.handleDeleteTodo}
-              /> 
-            </section>
-        </div>
-    );
+    <TodoProvider>
+      <TodoTemplate/>
+    </TodoProvider>
 };
