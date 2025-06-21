59a1c83be7d36865897e2ece3c71a357d65cc146
megane575 <rie-fttm0129@gmail.com>
2025-06-22 00:07:01 +0900

    main.jsxにProviderオブジェクトを設定に修正

--- first-react-app/src/main.jsx
+++ first-react-app/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { App } from './App.jsx'
+import { Router } from '../router'
+import { TodoProvider } from './contexts/TodoContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TodoProvider>
+      <Router />
+    </TodoProvider>    
   </StrictMode>,
 )
