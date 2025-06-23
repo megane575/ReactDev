e076e6cfe362d838fa3337f552c822a57d921de9
megane575 <rie-fttm0129@gmail.com>
2025-06-23 23:35:51 +0900

    BseLayoutコンポーネントとcssの作成

---
+++ first-react-app/src/components/organisms/BaseLayout/BaseLayout.jsx
@@ -0,0 +1,12 @@
+import { Navigation } from '../../../molecules/Navigation';
+import './style.css';
+
+export const BaseLayout = ({children,title}) => (
+ <div className='container'>
+    <section className='common'>
+     <Navigation/>
+    </section>
+    <h1 className='title'>{title}</h1>
+    {children}
+ </div>
+);
