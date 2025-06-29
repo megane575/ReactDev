4941e9698d1fc6b1bfab8164251d9d3530d4e60d
megane575 <rie-fttm0129@gmail.com>
2025-06-29 22:15:32 +0900

    #TextAreaファイルの追加#

---
+++ first-react-app/src/components/atoms/TextArea/TextArea.jsx
@@ -0,0 +1,14 @@
+export const TextArea = ({
+    desabled=false,
+    value,
+    placeholder,
+    onChange,
+}) => (
+ <textarea 
+    desabled={desabled}
+    className="text"
+    value={value}
+    placeholder={placeholder}
+    onChange={onChange}
+ />
+);
